cb721a1776904a70a81a1e707f04d52314037d59
The-Kian <kian.popat@gmail.com>
2022-09-26 13:14:55 +0100

    trying some things with some data

--- map/main.js
+++ map/main.js
@@ -1,17 +1,31 @@
-import './style.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+//Import stuff from openmap api
 
-const map = new Map({
-  target: 'map',
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import {Map, View} from 'ol';
+import {fromLonLat} from 'ol/proj';
+import GeoJSON from 'ol/format/GeoJSON';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+
+
+new Map({ //new map
+  target: 'map-container', // throw it in map-container
   layers: [
     new TileLayer({
-      source: new OSM()
+      source: new OSM(),
+    }),
+    new VectorLayer({ //
+      source: new VectorSource({
+        source: new VectorSource({
+          format: new GeoJSON(),
+          url: './data/countries.json'
+        })
+      })
     })
   ],
   view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
+    center: fromLonLat([	-1.548567, 	53.801277]),
+    zoom: 12,
+  }),
 });
